9a78fd58ad76ee71ec80eefa84f6d354b6b8ea56
raj <raj@gmail.com>
2023-03-14 16:22:33 +0530

    modification of test and using annotation in hibernate

--- src/main/java/com/example/Hibernatepart1/Employee.java
+++ src/main/java/com/example/Hibernatepart1/Employee.java
@@ -1,15 +1,22 @@
 package com.example.Hibernatepart1;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import lombok.Data;
+
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
+@Entity
+@Table(name = "employee")
 public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy =GenerationType.AUTO)
     private int id ;
+    @Column
     private String name;
+    @Column
     private int age;
 
 
--- src/main/java/com/example/Hibernatepart1/TestEmployee.java
+++ src/main/java/com/example/Hibernatepart1/TestEmployee.java
@@ -1,18 +1,74 @@
 package com.example.Hibernatepart1;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TestEmployee {
-    public static void main(String[] args) {
-        SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
-        Session session =sessionFactory.openSession();
-        session.beginTransaction();
+    private static SessionFactory sessionFactory = null;
 
-      Employee employee =new Employee("rajdhar",23);
-      session.save(employee);
-      session.getTransaction().commit();
-      session.close();
+    static {
+        try {
+            loadSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+    public static void loadSessionFactory() {
+
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+
+        configuration.addAnnotatedClass(com.example.Hibernatepart1.Employee.class);
+
+
+        ServiceRegistry srvcReg = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+                .build();
+        sessionFactory = configuration.buildSessionFactory(srvcReg);
+    }
+    public static Session getSession() throws HibernateException {
+
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+        } catch (Exception e) {
+            System.err.println("Exception while opening a session ");
+            e.printStackTrace();
+        }
+
+        return session;
+    }
+
+    public static void main(String[] args) {
+        Session session = null;
+        Transaction txn = null;
+
+        try {
+            session = getSession();
+            txn = session.beginTransaction();
+
+      Employee employee=new Employee("raj",55);
+
+
+
+            session.save(employee);
+
+            txn.commit();
+
+        } catch (Exception e) {
+            if (txn != null)
+                txn.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+    }
+
